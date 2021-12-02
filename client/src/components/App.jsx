183f6eff2a601d76241c4194b47a21dcf600c30d
mhhutton <mhhutton@gmail.com>
2021-12-01 17:25:02 -0700

    Updates eslint file and JSX file

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="title-text">This is your Application!</div>
+      <img src="react-logo.png" alt="React Logo" />
+    </div>
+  );
+}
+
+export default App;
