a5548cd9eb60cd7f45d544a1deb60ece5f273f2b
mhhutton <mhhutton@gmail.com>
2021-12-01 17:26:07 -0700

    Rearranges server files and added body parser

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../client/dist')))
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(bodyParser.json());
 
 module.exports = app;
